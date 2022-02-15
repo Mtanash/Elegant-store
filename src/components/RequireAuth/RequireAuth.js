d0f8ce6eb9b74e6b4be7c984e017ac7fbac11305
Mohamed Tanash <tanashkoo@gmail.com>
2022-02-15 02:09:19 +0200

    add require auth component to control protected routes

---
+++ src/components/RequireAuth/RequireAuth.js
@@ -0,0 +1,17 @@
+import { useLocation, Navigate, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectCurrentUser } from "../../features/user/userSlice";
+
+const RequireAuth = ({ allowedRoles }) => {
+  const user = useSelector(selectCurrentUser)?.user;
+  const location = useLocation();
+  return user?.role && allowedRoles.includes(user?.role) ? (
+    <Outlet />
+  ) : user ? (
+    <Navigate to="/unauthorized" state={{ from: location }} replace />
+  ) : (
+    <Navigate to="/auth" state={{ from: location }} replace />
+  );
+};
+
+export default RequireAuth;
