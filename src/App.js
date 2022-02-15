ee1a334e762d08c39d2bcc8af23cdc1300f92997
Mohamed Tanash <tanashkoo@gmail.com>
2022-02-15 02:10:22 +0200

    add protedcted toutes add not found page add not authorized page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import AuthPage from "./components/AuthPage/AuthPage";
 import Home from "./components/Home/Home";
 import CreateProduct from "./components/CreateProduct/CreateProduct";
 import ProfilePage from "./components/ProfilePage/ProfilePage";
+import DashboardPage from "./components/DashboardPage/DashboardPage";
+import RequireAuth from "./components/RequireAuth/RequireAuth";
+import UnauthorizedPage from "./components/UnauthorizedPage/UnauthorizedPage";
+import NotFoundPage from "./components/NotFoundPage/NotFoundPage";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -15,11 +19,20 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/me" element={<ProfilePage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/auth" element={<AuthPage />} />
-        <Route path="/product/:id" element={<ProductPage />} />
-        <Route path="/create-product" element={<CreateProduct />} />
+        <Route path="/unauthorized" element={<UnauthorizedPage />} />
+
+        <Route element={<RequireAuth allowedRoles={["admin"]} />}>
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/create-product" element={<CreateProduct />} />
+        </Route>
+        <Route element={<RequireAuth allowedRoles={["admin", "user"]} />}>
+          <Route path="/me" element={<ProfilePage />} />
+        </Route>
+
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
