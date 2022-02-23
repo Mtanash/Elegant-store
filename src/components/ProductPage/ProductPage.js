d3fb730964ac6ace46c7e8c990656ebe9de45cf3
Mohamed Tanash <tanashkoo@gmail.com>
2022-02-23 12:39:38 +0200

    refactor product page component to use material ui

--- src/components/ProductPage/ProductPage.js
+++ src/components/ProductPage/ProductPage.js
@@ -1,17 +1,25 @@
 import { useEffect, useState } from "react";
+import LoadingPage from "../LoadingPage/LoadingPage";
 
-import { useDispatch, useSelector } from "react-redux";
-import { selectProductById } from "../../features/products/productsSlice";
+import { useDispatch } from "react-redux";
 import { productAddedToCart } from "../../features/Cart/cartSlice";
+import { fetchProductById } from "../../api/productsApi";
 
 import { useParams } from "react-router-dom";
 
-import Rating from "@mui/material/Rating";
-import { Button, TextField } from "@mui/material";
+import moment from "moment";
 
-import "../../css/ProductPage/ProductPage.css";
-import { fetchProductById } from "../../api/productsApi";
-import { Box } from "@mui/system";
+import {
+  Button,
+  TextField,
+  Rating,
+  Box,
+  Grid,
+  Typography,
+  Divider,
+  Paper,
+  Container,
+} from "@mui/material";
 
 const ProductPage = () => {
   const dispatch = useDispatch();
@@ -40,40 +48,77 @@ const ProductPage = () => {
       productAddedToCart({ productToAdd: product, amount: productQuantity })
     );
   };
-  if (loading) return <Box>Loading</Box>;
+
+  if (loading) return <LoadingPage fullHeight={true} />;
   else
     return (
-      <section className="product-page">
-        <div className="product-img">
-          <img src={product?.imageUrl} alt={product?.description} />
-        </div>
-        <div className="product-info">
-          <h3 className="product-title">{product?.description}</h3>
-          <Rating
-            className="product-rating"
-            size="small"
-            name="read-only"
-            value={product?.rating}
-            readOnly
-          />
-          <p className="product-price">${product?.price}</p>
-          <TextField
-            sx={{ width: "120px" }}
-            label="Quantity"
-            type="number"
-            size="small"
-            value={productQuantity}
-            onChange={(e) => setProductQuantity(parseInt(e.target.value))}
-          />
-          <Button variant="contained" onClick={onAddToCartClick}>
-            Add to cart
-          </Button>
-        </div>
-        <div className="product-description">
-          <h4>Description</h4>
-          <p>{product?.description}</p>
-        </div>
-      </section>
+      <Container sx={{ minHeight: "calc(100vh - 124px)" }}>
+        <Box sx={{ paddingTop: "1px" }}>
+          <Grid container spacing={1} sx={{ marginTop: "0" }}>
+            <Grid item xs={3}>
+              <img src={product?.imageUrl} alt={product?.description} />
+            </Grid>
+            <Grid item xs={6}>
+              <Typography gutterBottom variant="body1">
+                {product.description}
+              </Typography>
+              <Rating
+                size="small"
+                name="read-only"
+                value={product?.rating}
+                readOnly
+              />
+              <Divider sx={{ margin: "5px 0" }} />
+              <Typography variant="h6">Price: ${product.price}.00</Typography>
+              <Typography variant="caption" gutterBottom>
+                All prices include VAT.
+              </Typography>
+              <Divider sx={{ margin: "5px 0" }} />
+              <Typography variant="h6">Details</Typography>
+              <Typography variant="body2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+                veniam ipsam cum tenetur quia modi debitis cumque tenetur quia
+                modi debitis cumque.
+              </Typography>
+            </Grid>
+            <Grid item xs={3} sx={{ padding: "10px" }}>
+              <Paper
+                variant="outlined"
+                sx={{
+                  padding: "10px",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "flex-start",
+                }}
+              >
+                <Typography variant="h6">${product.price}.00</Typography>
+                <Typography variant="body1">
+                  Delivery:{" "}
+                  {moment(new Date()).add(2, "days").format("dddd, MMM.D")}
+                </Typography>
+                <Typography variant="h6">In stock.</Typography>
+                <Divider sx={{ margin: "5px 0" }} />
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  sx={{ alignSelf: "center", margin: "30px 0" }}
+                >
+                  Add to cart
+                </Button>
+              </Paper>
+            </Grid>
+            <Grid item xs={12}>
+              <Divider sx={{ margin: "5px 0" }} />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="subtitle1" gutterBottom>
+                Customers also viewed
+              </Typography>
+              <Box>Coming soon</Box>
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
     );
 };
 
