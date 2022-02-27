28cf3a29f61f261444e8a1bb7dbb5eb5352ddbf0
Mohamed Tanash <tanashkoo@gmail.com>
2022-02-27 14:47:15 +0200

    add carousel component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,46 @@
 // import Filter from "../Filter/Filter";
 import Products from "../Products/Products";
 
-import { Typography, Button } from "@mui/material";
-import { Box } from "@mui/system";
+import {
+  Typography,
+  Button,
+  Box,
+  Paper,
+  Container,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+} from "@mui/material";
 
 import "../../css/Home/Home.css";
+import { useEffect, useState } from "react";
+import { getFeaturedProducts } from "../../api/productsApi";
+import Carousel from "react-material-ui-carousel";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const [featuredProducts, setFeaturedProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchFeaturedProducts = async () => {
+      setLoading(true);
+      try {
+        const response = await getFeaturedProducts();
+        setFeaturedProducts(response?.data.products);
+      } catch (err) {
+        console.log(err?.response?.data);
+      }
+      setLoading(false);
+    };
+
+    fetchFeaturedProducts();
+  }, []);
+
   return (
     <main className="home">
       <Box className="main-header">
@@ -43,8 +77,54 @@ const Home = () => {
           Shop Now
         </Button>
       </Box>
-      <Products />
       {/* <Filter /> */}
+      <Container>
+        <Carousel
+          animation="slide"
+          NextIcon={<ArrowForwardIosIcon />}
+          PrevIcon={<ArrowBackIosNewIcon />}
+          sx={{
+            "&>div:nth-child(1)": { display: "grid", placeItems: "center" },
+            marginTop: "25px",
+          }}
+        >
+          {featuredProducts.map((product) => (
+            <Card raised key={product._id}>
+              <CardActionArea
+                onClick={() => navigate(`/product/${product._id}`)}
+                sx={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)" }}
+              >
+                <CardMedia
+                  component="img"
+                  height="200"
+                  image={product.imageUrl}
+                  alt={product.description}
+                  sx={{ objectFit: "contain" }}
+                />
+                <CardContent sx={{ gridColumn: "2/4" }}>
+                  <Typography
+                    variant="h6"
+                    gutterBottom
+                    align="center"
+                    sx={{
+                      height: "95px",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    {product.description}
+                  </Typography>
+                  <Typography variant="body1" component="div" align="center">
+                    ${product.price}.00
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))}
+        </Carousel>
+        <Products />
+      </Container>
     </main>
   );
 };
