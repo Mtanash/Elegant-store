554baa05ebe9672d9c1cf5996a167137c2638534
Mohamed Tanash <tanashkoo@gmail.com>
2022-03-02 14:14:33 +0200

    add style fixing add button on product card navigating to product page

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -4,8 +4,14 @@ import {
   CardContent,
   Button,
   Typography,
+  Divider,
+  CardActionArea,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
+
+import { truncateString } from "../../utils";
+
+import { useNavigate } from "react-router-dom";
 
 const ProductCard = ({
   description,
@@ -15,22 +21,32 @@ const ProductCard = ({
   _id,
   onRemoveButtonClicked,
 }) => {
+  const navigate = useNavigate();
   return (
     <Card
       key={_id}
       sx={{ display: "flex", paddingRight: "10px", marginBottom: "15px" }}
       raised
     >
-      <CardMedia
-        component="img"
-        src={imageUrl}
-        alt={description}
-        sx={{ width: "100px", height: "100px" }}
-      />
-      <CardContent sx={{ flex: "1" }}>
-        <Typography variant="body2">Qty: {quantity}</Typography>
-        <Typography variant="body2">Price: ${price}</Typography>
-      </CardContent>
+      <CardActionArea
+        sx={{ display: "flex" }}
+        onClick={() => navigate(`/product/${_id}`)}
+      >
+        <CardMedia
+          component="img"
+          src={imageUrl}
+          alt={description}
+          sx={{ width: "100px", height: "100px" }}
+        />
+        <CardContent sx={{ flex: "1" }}>
+          <Typography variant="h6">
+            {truncateString(description, 50)}
+          </Typography>
+          <Divider />
+          {quantity && <Typography variant="body2">Qty: {quantity}</Typography>}
+          <Typography variant="body2">Price: ${price}</Typography>
+        </CardContent>
+      </CardActionArea>
       <Button
         sx={{ alignSelf: "center" }}
         variant="outlined"
