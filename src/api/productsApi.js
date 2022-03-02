f76588dfba6044609befea4a18d2f9d2b154a4dc
Mohamed Tanash <tanashkoo@gmail.com>
2022-03-02 14:19:50 +0200

    add product image uploading api calls

--- src/api/productsApi.js
+++ src/api/productsApi.js
@@ -6,10 +6,16 @@ export const fetchProducts = (page = 1) =>
 export const fetchProductById = (id) => publicAxios.get(`products/${id}`);
 
 export const getProductsBySearch = (searchQuery) =>
-  publicAxios.get(`products/search?searchQuery=${searchQuery}`);
+  publicAxios.get(`products?search=${searchQuery}`);
 
 export const addProduct = (productData) =>
   privateAxios.post("products/", productData);
 
 export const getFeaturedProducts = () =>
   publicAxios.get("products?featured=true");
+
+export const getImageUploadUrl = (imageName) =>
+  publicAxios.get(`products/imageUploadUrl/${imageName}`);
+
+export const addProductImageUrl = (data) =>
+  publicAxios.post("products/image", data);
