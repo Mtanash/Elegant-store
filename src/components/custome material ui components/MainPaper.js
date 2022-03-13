807d44a0330c16f6654d45f2fca0c7d052b9e933
Mtanash <tanashkoo@gmail.com>
2022-03-13 16:32:08 +0200

    add custome material ui paper component

---
+++ src/components/custome material ui components/MainPaper.js
@@ -0,0 +1,13 @@
+import { Paper, styled } from "@mui/material";
+
+const MainPaper = styled(Paper)(() => ({
+  width: "100%",
+  height: "100%",
+  padding: "15px",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+export default MainPaper;
