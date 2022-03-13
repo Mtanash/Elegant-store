3125121d33aee5aef950a4383a3f927a26892259
Mtanash <tanashkoo@gmail.com>
2022-03-13 16:32:58 +0200

    add ProfileOrdersPage component

---
+++ src/components/ProfilePage/ProfileOrdersPage.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import Orders from "../Orders/Orders";
+import LoadingPage from "../LoadingPage/LoadingPage";
+import useAxios from "../../hooks/useAxios";
+import { privateAxios } from "../../api/axios";
+import ErrorPage from "../ErrorPage/ErrorPage";
+
+const ProfileOrdersPage = () => {
+  const [orders, ordersLoading, ordersError, axiosFetch] = useAxios();
+
+  useEffect(() => {
+    axiosFetch({
+      axiosInstance: privateAxios,
+      method: "GET",
+      url: "users/me/orders",
+    });
+  }, []);
+
+  if (ordersLoading) return <LoadingPage />;
+  else if (ordersError) return <ErrorPage />;
+  else return <Orders orders={orders} title="My Orders" />;
+};
+
+export default ProfileOrdersPage;
