9c13be8c99fdc5d134c30c26948eeb2cc1d451e5
Mtanash <tanashkoo@gmail.com>
2022-03-13 16:36:23 +0200

    refactor UserAvatar component add logic to get current user if no user passed as prop

--- src/components/UserAvatar/UserAvatar.js
+++ src/components/UserAvatar/UserAvatar.js
@@ -1,6 +1,12 @@
 import { Avatar } from "@mui/material";
+import { useSelector } from "react-redux";
+import { selectCurrentUser } from "../../features/user/userSlice";
 
 const UserAvatar = ({ user, styles }) => {
+  const currentUser = useSelector(selectCurrentUser);
+
+  if (!user) user = currentUser;
+
   return user?.avatar ? (
     <Avatar src={user?.avatar} size="small" sx={styles} />
   ) : (
