9f268bf0308aee5e37eb980f9cf6457d1d9b7e36
Mtanash <tanashkoo@gmail.com>
2022-03-15 11:58:26 +0200

    remove order reducer

--- src/app/store.js
+++ src/app/store.js
@@ -2,14 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from "../features/products/productsSlice";
 import cartReducer from "../features/Cart/cartSlice";
 import userReducer from "../features/user/userSlice";
-import orderReducer from "../features/order/orderSlice";
 
 const store = configureStore({
   reducer: {
     products: productsReducer,
     cart: cartReducer,
     user: userReducer,
-    order: orderReducer,
   },
 });
 
