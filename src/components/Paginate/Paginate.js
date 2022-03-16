c27a15bb352378785c8cc8657b43abdfd83325a5
Mtanash <tanashkoo@gmail.com>
2022-03-16 17:37:23 +0200

    remove loading and error props

--- src/components/Paginate/Paginate.js
+++ src/components/Paginate/Paginate.js
@@ -1,35 +1,25 @@
 import { Box, Pagination } from "@mui/material";
 
-import ProductsComponent from "../Products/ProductsComponent";
-import LoadingPage from "../LoadingPage/LoadingPage";
-import ErrorPage from "../ErrorPage/ErrorPage";
+import ProductsContainer from "../Products/ProductsContainer";
+import LoadingPage from "../../pages/LoadingPage/LoadingPage";
+import ErrorPage from "../../pages/ErrorPage/ErrorPage";
 
-const Paginate = ({
-  loading,
-  error,
-  products,
-  totalPages,
-  page,
-  handlePageChange,
-}) => {
-  if (loading) return <LoadingPage />;
-  else if (error) return <ErrorPage />;
-  else
-    return (
-      <Box>
-        <ProductsComponent products={products} />
-        <Pagination
-          sx={{ padding: "15px 0", "& > ul": { justifyContent: "center" } }}
-          count={totalPages}
-          page={page}
-          onChange={handlePageChange}
-          showLastButton
-          size="large"
-          color="primary"
-          shape="rounded"
-        />
-      </Box>
-    );
+const Paginate = ({ products, totalPages, page, handlePageChange }) => {
+  return (
+    <Box>
+      <ProductsContainer products={products} />
+      <Pagination
+        sx={{ padding: "15px 0", "& > ul": { justifyContent: "center" } }}
+        count={totalPages}
+        page={page}
+        onChange={handlePageChange}
+        showLastButton
+        size="large"
+        color="primary"
+        shape="rounded"
+      />
+    </Box>
+  );
 };
 
 export default Paginate;
