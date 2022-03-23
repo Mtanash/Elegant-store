b837a5fc56d362897f8457dbd87c007c9a1906f0
Mohamed Tanash <tanashkoo@gmail.com>
2022-03-23 17:08:11 +0200

    Update Rates.js
    remove fetch request to get product rates
    get rates as a prop from parent component
    calculate total rates and rate to display them

--- src/components/Rates/Rates.js
+++ src/components/Rates/Rates.js
@@ -1,39 +1,33 @@
-import { Box, LinearProgress, Rating, Typography } from "@mui/material";
-import { useEffect } from "react";
-import useAxios from "../../hooks/useAxios";
-import { publicAxios } from "../../api/axios";
+import { Box, Rating, Typography } from "@mui/material";
 
-const Rates = ({ productId, rerender }) => {
-  const [
-    { productRates, totalRates, totalRatesValue },
-    ratesLoading,
-    ratesError,
-    fetchProductRatesData,
-  ] = useAxios();
+const Rates = ({ rates = [], center }) => {
+  const { value: totalRatesValue } = rates.reduce(
+    (a, b) => ({
+      value: a.value + b.value,
+    }),
+    { value: 0 }
+  );
+  const totalRates = rates.length;
+  const rate = Math.ceil(totalRatesValue / totalRates);
 
-  useEffect(() => {
-    if (!productId) return;
-    fetchProductRatesData({
-      axiosInstance: publicAxios,
-      method: "GET",
-      url: `products/rates/${productId}`,
-    });
-  }, [productId, rerender]);
-
-  if (ratesLoading) return <LinearProgress />;
-  if (ratesError) return <p>Failed to load rate</p>;
-  else
-    return (
-      <Box sx={{ display: "flex", gap: "5px", alignItems: "center" }}>
-        <Rating
-          size="small"
-          name="read-only"
-          value={Math.floor(totalRatesValue / totalRates)}
-          readOnly
-        />
-        <Typography>{totalRates}</Typography>
-      </Box>
-    );
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        gap: "5px",
+        alignItems: "center",
+        justifyContent: center ? "center" : "flex-start",
+        height: "24px",
+      }}
+    >
+      {rate > 0 && (
+        <>
+          <Rating size="small" name="read-only" value={rate} readOnly />
+          <Typography>{totalRates}</Typography>
+        </>
+      )}
+    </Box>
+  );
 };
 
 export default Rates;
