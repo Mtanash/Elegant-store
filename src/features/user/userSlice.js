309a5ee924f305bcde479c120fadd8bc0ab28608
Mohamed Tanash <tanashkoo@gmail.com>
2022-03-23 17:23:24 +0200

    Update userSlice.js
    remove login async thunk function
    remove signup async thunk function
    remove updateUserAvatar async thunk function
    remove authWithGoogle async thunk function
    add userLoggedIn reducer
    add accessTokenRefreshed reducer
    add userDataRefresshed reducer
    remove extra reducers

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,69 +1,27 @@
-import {
-  createSlice,
-  createAsyncThunk,
-  isFulfilled,
-  isRejectedWithValue,
-} from "@reduxjs/toolkit";
-import { login, signup, updateAvatar, googleAuth } from "../../api/userApi";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  user: localStorage.getItem("user")
-    ? JSON.parse(localStorage.getItem("user"))
-    : null,
-  error: null,
+  user: null,
+  accessToken: null,
 };
-
-export const loginUser = createAsyncThunk(
-  "user/loginUser",
-  async (userData, { rejectWithValue }) => {
-    try {
-      const response = await login(userData);
-      return response?.data;
-    } catch (err) {
-      return rejectWithValue(err?.response?.data);
-    }
-  }
-);
-
-export const signupUser = createAsyncThunk(
-  "user/signupUser",
-  async (userData, { rejectWithValue }) => {
-    try {
-      const response = await signup(userData);
-      return response?.data;
-    } catch (err) {
-      return rejectWithValue(err?.response?.data);
-    }
-  }
-);
-
-export const updateUserAvatar = createAsyncThunk(
-  "user/updateUserAvatar",
-  async (avatar) => {
-    await updateAvatar(avatar);
-    return avatar;
-  }
-);
-
-export const authWithGoogle = createAsyncThunk(
-  "user/authWithGoogle",
-  async (data, { rejectWithValue }) => {
-    try {
-      const response = await googleAuth(data);
-      return response?.data;
-    } catch (err) {
-      return rejectWithValue(err?.response?.data);
-    }
-  }
-);
 
 const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    logoutUser: (state) => {
+    userLoggedIn: (state, action) => {
+      state.user = action.payload.user;
+      state.accessToken = action.payload.accessToken;
+    },
+    accessTokenRefreshed: (state, action) => {
+      state.accessToken = action.payload;
+    },
+    userDataRefreshed: (state, action) => {
+      state.user = action.payload;
+    },
+    userLoggedOut: (state) => {
       state.user = null;
-      localStorage.setItem("user", null);
+      state.accessToken = null;
     },
     productAddedToFavorite: (state, action) => {
       state.user.favoriteProducts.push(action.payload._id);
@@ -74,45 +32,16 @@ const userSlice = createSlice({
       );
       state.user.favoriteProducts = newFavoriteProducts;
     },
-    userSavedToLocalStorage: (state) => {
-      localStorage.setItem("user", JSON.stringify(state.user));
-    },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(loginUser.fulfilled, (state, action) => {
-        state.user = action.payload.user;
-      })
-      .addCase(signupUser.fulfilled, (state, action) => {
-        state.user = action.payload.user;
-      })
-      .addCase(authWithGoogle.fulfilled, (state, action) => {
-        state.user = action.payload.user;
-      })
-      .addCase(updateUserAvatar.fulfilled, (state, action) => {
-        state.user.avatar = action.payload.avatar;
-      })
-      .addMatcher(
-        isFulfilled(loginUser, signupUser, authWithGoogle),
-        (state) => {
-          localStorage.setItem("user", JSON.stringify(state.user));
-          state.error = null;
-        }
-      )
-      .addMatcher(
-        isRejectedWithValue(loginUser, signupUser, authWithGoogle),
-        (state, action) => {
-          state.error = action.payload.message;
-        }
-      );
   },
 });
 
 export const {
-  logoutUser,
+  userLoggedOut,
   productAddedToFavorite,
   productRemovedFromFavorite,
-  userSavedToLocalStorage,
+  userLoggedIn,
+  accessTokenRefreshed,
+  userDataRefreshed,
 } = userSlice.actions;
 
 export const selectCurrentUser = (state) => state.user.user;
