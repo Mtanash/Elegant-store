508bd624b0e4aa61dd334e8c1d482633ac785a23
Mohamed Tanash <tanashkoo@gmail.com>
2022-03-23 17:06:14 +0200

    Update VerticalProductCard.js
    remove handleAddToFavorite custom hook instance
    get handleAddToFavorite function as a prop from parent component
    add price component
    add rates component
    remove snackbar component

--- src/components/VerticalProductCard/VerticalProductCard.js
+++ src/components/VerticalProductCard/VerticalProductCard.js
@@ -1,13 +1,10 @@
-import { useState } from "react";
 import { truncateString } from "../../utils";
 
 import { useDispatch, useSelector } from "react-redux";
 import { productAddedToCart } from "../../features/Cart/cartSlice";
 
-import CloseIcon from "@mui/icons-material/Close";
 import {
   Button,
-  Alert,
   Card,
   CardActions,
   CardContent,
@@ -15,8 +12,6 @@ import {
   Divider,
   Typography,
   CardActionArea,
-  IconButton,
-  Snackbar,
 } from "@mui/material";
 import LoadingButton from "@mui/lab/LoadingButton";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
@@ -24,120 +19,86 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 
 import { useNavigate } from "react-router-dom";
-import useHandleAddToFavorite from "../../hooks/useHandleAddToFavorite";
+import Price from "../Price/Price";
+import Rates from "../Rates/Rates";
 
-function VerticalProductCard({ product }) {
-  const { description, price, imageUrl, _id } = product;
-  const [snackbarOpen, setSnackbarOpen] = useState(false);
-  const [addToFavoriteLoading, handleAddToFavorite] = useHandleAddToFavorite();
+function VerticalProductCard({
+  product,
+  addToFavoriteLoading,
+  handleAddToFavorite,
+}) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { description, price, priceAfterDiscount, imageUrl, _id, rates } =
+    product;
+
   const productIsFavorite = useSelector((state) =>
     state.user.user?.favoriteProducts.includes(_id)
   );
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const openSnackbar = () => {
-    setSnackbarOpen(true);
-  };
-
-  const onSnackbarClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-    setSnackbarOpen(false);
-  };
-
   const onAddToCartButtonClicked = () => {
     dispatch(productAddedToCart({ productToAdd: product }));
-    openSnackbar();
   };
 
-  const action = (
-    <IconButton
-      size="small"
-      aria-label="close"
-      color="inherit"
-      onClick={onSnackbarClose}
-    >
-      <CloseIcon fontSize="small" />
-    </IconButton>
-  );
-
   return (
-    <>
-      <Card
-        raised
+    <Card
+      raised
+      sx={{
+        margin: "10px",
+        width: "250px",
+      }}
+    >
+      <CardActionArea onClick={() => navigate(`/product/${product._id}`)}>
+        <CardMedia
+          component="img"
+          height="200"
+          image={imageUrl}
+          alt="white"
+          sx={{ objectFit: "contain" }}
+        />
+        <CardContent>
+          <Typography
+            variant="body2"
+            gutterBottom
+            align="center"
+            sx={{
+              height: "95px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            {truncateString(description)}
+          </Typography>
+          <Divider sx={{ margin: "5px 0" }} />
+          <Price price={price} priceAfterDiscount={priceAfterDiscount} center />
+          <Rates rates={rates} center />
+        </CardContent>
+      </CardActionArea>
+      <CardActions
         sx={{
-          margin: "10px",
-          width: "250px",
+          display: "flex",
+          justifyContent: "center",
         }}
       >
-        <CardActionArea onClick={() => navigate(`/product/${product._id}`)}>
-          <CardMedia
-            component="img"
-            height="200"
-            image={imageUrl}
-            alt="white"
-            sx={{ objectFit: "contain" }}
-          />
-          <CardContent>
-            <Typography
-              variant="body2"
-              gutterBottom
-              align="center"
-              sx={{
-                height: "95px",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              {truncateString(description)}
-            </Typography>
-            <Divider sx={{ margin: "5px 0" }} />
-            <Typography variant="h5" component="div" align="center">
-              ${price}.00
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-          }}
+        <Button aria-label="favorite" onClick={() => handleAddToFavorite(_id)}>
+          {productIsFavorite ? (
+            <FavoriteIcon sx={{ color: "red" }} />
+          ) : (
+            <FavoriteBorderIcon />
+          )}
+        </Button>
+        <Button
+          size="small"
+          variant="contained"
+          endIcon={<AddShoppingCartIcon />}
+          onClick={onAddToCartButtonClicked}
         >
-          <LoadingButton
-            aria-label="favorite"
-            loading={addToFavoriteLoading}
-            onClick={() => handleAddToFavorite(_id)}
-          >
-            {productIsFavorite ? (
-              <FavoriteIcon sx={{ color: "red" }} />
-            ) : (
-              <FavoriteBorderIcon />
-            )}
-          </LoadingButton>
-          <Button
-            size="small"
-            variant="contained"
-            endIcon={<AddShoppingCartIcon />}
-            onClick={onAddToCartButtonClicked}
-          >
-            Add to cart
-          </Button>
-        </CardActions>
-      </Card>
-      <Snackbar
-        open={snackbarOpen}
-        onClose={onSnackbarClose}
-        autoHideDuration={4000}
-        action={action}
-      >
-        <Alert onClose={onSnackbarClose} variant="filled" severity="success">
-          Product added to cart!
-        </Alert>
-      </Snackbar>
-    </>
+          Add to cart
+        </Button>
+      </CardActions>
+    </Card>
   );
 }
 
