064fab8c9fc13523d86592bcc4dd2b60c650bb15
Mohamed Tanash <tanashkoo@gmail.com>
2022-03-25 14:27:08 +0200

    remove products reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import productsReducer from "../features/products/productsSlice";
 import cartReducer from "../features/Cart/cartSlice";
 import userReducer from "../features/user/userSlice";
 
 const store = configureStore({
   reducer: {
-    products: productsReducer,
     cart: cartReducer,
     user: userReducer,
   },
