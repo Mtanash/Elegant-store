0b0ddde7c086a1004619b0128d627881e738df88
Mtanash <tanashkoo@gmail.com>
2022-04-01 15:15:09 +0200

    add discount option to add product form optimize styling

--- src/components/AddProductForm/AddProductForm.js
+++ src/components/AddProductForm/AddProductForm.js
@@ -1,19 +1,29 @@
-import { Input, TextField, Typography } from "@mui/material";
+import {
+  Input,
+  TextField,
+  Typography,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@mui/material";
 import LoadingButton from "@mui/lab/LoadingButton";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 import { useState, useContext } from "react";
 import Resizer from "react-image-file-resizer";
-import "../../css/AddProductForm/AddProductForm.css";
 import usePrivateAxios from "../../hooks/usePrivateAxios";
 import { publicAxios } from "../../api/axios";
 import SnackbarContext from "../../context/SnackbarContext";
+import MainPaper from "../custome material ui components/MainPaper";
+import { categoriesObj } from "../../constants";
 
 const initialFormData = {
   description: "",
   price: "",
   stock: "1",
   featured: false,
+  category: categoriesObj.womenClothes,
 };
 
 const AddProductForm = () => {
@@ -22,6 +32,8 @@ const AddProductForm = () => {
   const [file, setFile] = useState(null);
   const privateAxios = usePrivateAxios();
   const [loading, setLoading] = useState(false);
+  const [productHaveDiscount, setProductHaveDiscount] = useState(false);
+  const [priceAfterDiscount, setPriceAfterDiscount] = useState("");
 
   const onFormDataChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -30,6 +42,16 @@ const AddProductForm = () => {
   const onFormSubmit = (e) => {
     e.preventDefault();
     setLoading(true);
+
+    // if product have discount => add price after discount to formData obj
+    if (productHaveDiscount) {
+      Object.defineProperty(formData, "priceAfterDiscount", {
+        value: priceAfterDiscount,
+        writable: true,
+        enumerable: true,
+        configurable: true,
+      });
+    }
 
     const uploadProductImage = async (imageName) => {
       try {
@@ -82,7 +104,7 @@ const AddProductForm = () => {
   };
 
   return (
-    <div className="addProductForm">
+    <MainPaper elevation={3} sx={{ width: "100%" }}>
       <Typography
         variant="h4"
         component="h2"
@@ -92,7 +114,16 @@ const AddProductForm = () => {
       >
         Add new product
       </Typography>
-      <form onSubmit={onFormSubmit}>
+      <form
+        onSubmit={onFormSubmit}
+        style={{
+          width: "70%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          gap: "10px",
+        }}
+      >
         <TextField
           required
           fullWidth
@@ -104,15 +135,61 @@ const AddProductForm = () => {
           value={formData.description}
           onChange={onFormDataChange}
         />
-        <TextField
-          required
-          fullWidth
-          type="number"
-          label="Price"
-          name="price"
-          value={formData.price}
-          onChange={onFormDataChange}
+        <FormControl fullWidth>
+          <InputLabel id="category-select">Category</InputLabel>
+          <Select
+            labelId="category-select"
+            value={formData.category}
+            label="Category"
+            name="category"
+            onChange={onFormDataChange}
+            required
+          >
+            {Object.values(categoriesObj).map((category) => (
+              <MenuItem key={category} value={category}>
+                {category}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControlLabel
+          control={<Checkbox />}
+          label="Product have discount"
+          checked={productHaveDiscount}
+          onChange={(e) => setProductHaveDiscount(e.target.checked)}
         />
+        {productHaveDiscount ? (
+          <>
+            <TextField
+              required
+              fullWidth
+              type="number"
+              label="Price before discount"
+              name="price"
+              value={formData.price}
+              onChange={onFormDataChange}
+            />
+            <TextField
+              required
+              fullWidth
+              type="number"
+              label="Price after discount"
+              name="priceAfterDiscount"
+              value={priceAfterDiscount}
+              onChange={(e) => setPriceAfterDiscount(e.target.value)}
+            />
+          </>
+        ) : (
+          <TextField
+            required
+            fullWidth
+            type="number"
+            label="Price"
+            name="price"
+            value={formData.price}
+            onChange={onFormDataChange}
+          />
+        )}
         <TextField
           required
           fullWidth
@@ -140,7 +217,7 @@ const AddProductForm = () => {
           Add product
         </LoadingButton>
       </form>
-    </div>
+    </MainPaper>
   );
 };
 
