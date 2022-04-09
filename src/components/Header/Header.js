b15c4dcb1df13846a69c6b335090d4cf5422bf6c
Mtanash <tanashkoo@gmail.com>
2022-04-09 17:05:47 +0200

    removed material ui components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,183 +1,73 @@
-import React, { useEffect, useState } from "react";
-import UserAvatar from "../UserAvatar/UserAvatar";
-
-import {
-  IconButton,
-  Typography,
-  AppBar,
-  Toolbar,
-  Menu,
-  MenuItem,
-  ListItemIcon,
-  Badge,
-  Input,
-  Box,
-} from "@mui/material";
-import Logout from "@mui/icons-material/Logout";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import Login from "@mui/icons-material/Login";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import SearchIcon from "@mui/icons-material/Search";
-
-import { useNavigate } from "react-router-dom";
-
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { selectCurrentUser } from "../../features/user/userSlice";
-import useLogout from "../../hooks/useLogout";
-import {
-  selectCartProducts,
-  selectCartProductsCount,
-} from "../../features/Cart/cartSlice";
+import { selectCartProducts } from "../../features/Cart/cartSlice";
+import HamburgerMenu from "../HamburgerMenu/HamburgerMenu";
+import SearchBar from "../SearchBar/SearchBar";
+import CartIcon from "../CartIcon/CartIcon";
+import UserAvatarMenu from "../UserAvatarMenu/UserAvatarMenu";
 
-const MenuItems = ({ user, handleClose }) => {
-  const navigate = useNavigate();
-  const logout = useLogout();
-  if (user)
-    return (
-      <>
-        <MenuItem
-          onClick={() => {
-            navigate("/me");
-            handleClose();
-          }}
-        >
-          <ListItemIcon>
-            <AccountCircleIcon fontSize="small" />
-          </ListItemIcon>
-          Profile
-        </MenuItem>
-        {user?.role === "admin" && (
-          <MenuItem
-            onClick={() => {
-              navigate("/dashboard");
-              handleClose();
-            }}
-          >
-            <ListItemIcon>
-              <DashboardIcon fontSize="small" />
-            </ListItemIcon>
-            Dashboard
-          </MenuItem>
-        )}
-        <MenuItem
-          onClick={async () => {
-            await logout();
-            handleClose();
-          }}
-        >
-          <ListItemIcon>
-            <Logout fontSize="small" />
-          </ListItemIcon>
-          Logout
-        </MenuItem>
-      </>
-    );
-
-  return (
-    <>
-      <MenuItem
-        onClick={() => {
-          navigate("/auth");
-          handleClose();
-        }}
-      >
-        <ListItemIcon>
-          <Login fontSize="small" />
-        </ListItemIcon>
-        Login
-      </MenuItem>
-    </>
-  );
-};
+const links = [
+  {
+    text: "Fashion",
+    url: "#",
+  },
+  {
+    text: "Bags",
+    url: "#",
+  },
+  {
+    text: "Watches",
+    url: "#",
+  },
+  {
+    text: "Shoes",
+    url: "#",
+  },
+  {
+    text: "Perfumes",
+    url: "#",
+  },
+];
 
 function Header() {
-  const navigate = useNavigate();
-  const user = useSelector(selectCurrentUser);
-  const cartProductsCount = useSelector(selectCartProductsCount);
   const cartProducts = useSelector(selectCartProducts);
-
-  const [anchorElement, setAnchorElement] = useState(null);
-  const [searchQuery, setSearchQuery] = useState("");
-  const open = Boolean(anchorElement);
 
   useEffect(() => {
     localStorage.setItem("cartProducts", JSON.stringify(cartProducts));
   }, [cartProducts]);
 
-  const handleClick = (e) => {
-    setAnchorElement(e.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorElement(null);
-  };
-
-  const handleSearchFormSubmit = (e) => {
-    e.preventDefault();
-    if (!searchQuery) return;
-    navigate(`products-search?search=${searchQuery}`);
-    setSearchQuery("");
-  };
-
   return (
-    <AppBar position="static">
-      <Toolbar sx={{ width: "100%", gap: "20px" }}>
-        <Typography variant="h6" component="div" sx={{ flex: "0.2" }}>
-          <a href="/">Elegant</a>
-        </Typography>
-        <form onSubmit={handleSearchFormSubmit} style={{ flex: "0.5" }}>
-          <Input
-            sx={{
-              backgroundColor: "white",
-              borderRadius: "8px",
-              padding: "0 5px",
-            }}
-            type="search"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            disableUnderline
-            placeholder="What are you looking for?"
-            variant="outlined"
-            startAdornment={<SearchIcon />}
-            fullWidth
-          />
-        </form>
-        <Box
-          sx={{
-            flex: "0.3",
-            display: "flex",
-            justifyContent: "flex-end",
-            alignItems: "center",
-            gap: "15px",
-          }}
-        >
-          <IconButton aria-label="cart" onClick={() => navigate("/cart")}>
-            <Badge badgeContent={cartProductsCount} color="secondary">
-              <ShoppingCartIcon sx={{ color: "#FFF" }} />
-            </Badge>
-          </IconButton>
-          <IconButton
-            onClick={handleClick}
-            size="small"
-            aria-label="account menu of current user"
-            aria-controls={open ? "account-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-          >
-            <UserAvatar user={user} />
-          </IconButton>
-          <Menu
-            id="account-menu"
-            anchorEl={anchorElement}
-            open={open}
-            onClose={handleClose}
-            keepMounted
-          >
-            <MenuItems user={user} handleClose={handleClose} />
-          </Menu>
-        </Box>
-      </Toolbar>
-    </AppBar>
+    <header className="p-5 container mx-auto flex flex-row justify-between items-center gap-4">
+      <a href="." className="text-l md:text-2xl text-bold flex-1 md:flex-none">
+        Elegant Store
+      </a>
+      <nav className="flex items-center justify-center gap-3">
+        <div className="gap-3 items-center hidden md:flex">
+          {links.map((link) => (
+            <a
+              key={link.text}
+              className="p-2 font-semibold text-grey hover:text-deep-blue inline-block transition-colors ease-in-out duration-200"
+              href={link.url}
+            >
+              {link.text}
+            </a>
+          ))}
+        </div>
+
+        {/* menu */}
+        <HamburgerMenu links={links} />
+
+        {/* search bar */}
+        <SearchBar />
+      </nav>
+
+      <div className="flex items-center gap-3">
+        {/* cart icon */}
+        <CartIcon />
+        {/* user menu */}
+        <UserAvatarMenu />
+      </div>
+    </header>
   );
 }
 
