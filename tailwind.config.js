ba2ea5ff9677796d101bca90460fd294a969e19d
Mtanash <tanashkoo@gmail.com>
2022-04-09 17:04:35 +0200

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      "deep-orange": "#FF5F00",
+      "deep-red": "#B20600",
+      "deep-blue": "#00092C",
+      "pale-white": "#eeeeee",
+      grey: "#444",
+      white: "#fff",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
