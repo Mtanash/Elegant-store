dc2369e3e2e94b9e788fb94174d173bbaba0c721
Mtanash <tanashkoo@gmail.com>
2022-04-13 20:01:11 +0200

    refactor to use tailwind instead of material ui

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,16 +1,6 @@
 import { useEffect, useState } from "react";
 
-import {
-  Alert,
-  Avatar,
-  Button,
-  Grid,
-  TextField,
-  Typography,
-} from "@mui/material";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import { LoadingButton } from "@mui/lab";
-import GoogleIcon from "@mui/icons-material/Google";
+import { FcGoogle } from "react-icons/fc";
 
 import { useDispatch, useSelector } from "react-redux";
 import { selectCurrentUser, userLoggedIn } from "../../features/user/userSlice";
@@ -20,8 +10,6 @@ import useAxios from "../../hooks/useAxios";
 import { publicAxios } from "../../api/axios";
 
 import { GoogleLogin } from "react-google-login";
-
-import "../../css/AuthPage/AuthPage.css";
 
 const initialFormData = { name: "", email: "", password: "" };
 
@@ -133,81 +121,124 @@ const AuthPage = () => {
   };
 
   return (
-    <section className="authPage">
-      <Avatar>
-        <LockOutlinedIcon />
-      </Avatar>
-      <Typography component="h1" variant="h5">
-        {isSignup ? "Sign up" : "Sign in"}
-      </Typography>
-      {errorMessage && (
-        <Alert sx={{ width: "100%" }} severity="error">
-          {errorMessage}
-        </Alert>
-      )}
-      <form className="authPage-form" onSubmit={handleFormSubmit}>
-        {isSignup && (
-          <TextField
-            name="name"
-            label="Name"
-            value={formData.name}
-            onChange={handleFormChange}
-            autoFocus
-            fullWidth
-          />
+    <section className="min-h-[calc(100vh_-_theme(headerAndFooterHeight))] grid place-items-center">
+      <div className="container mx-auto rounded-lg bg-white drop-shadow-2xl p-6 py-8 flex flex-col items-center justify-center">
+        <p className="text-3xl font-semibold mb-10 text-center">
+          {!isSignup ? "Sign in to your account" : "Sign up a new account"}
+        </p>
+        {errorMessage && (
+          <div className="p-1 rounded-sm bg-pale-white">
+            <p className="text-red text-center">{errorMessage}</p>
+          </div>
         )}
-        <TextField
-          name="email"
-          label="Email Address"
-          value={formData.email}
-          onChange={handleFormChange}
-          type="email"
-          fullWidth
-        />
-        <TextField
-          name="password"
-          label="Password"
-          type="password"
-          onChange={handleFormChange}
-          value={formData.password}
-          fullWidth
-        />
-        <LoadingButton
-          loading={authDataLoading}
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="primary"
+        <form
+          className="flex flex-col gap-4 w-11/12 sm:w-8/12 lg:w-7/12 xl:w-5/12"
+          onSubmit={handleFormSubmit}
         >
-          {isSignup ? "Sign Up" : "Sign In"}
-        </LoadingButton>
-        <GoogleLogin
-          clientId="203406370220-3gj58q2161jl5b0u07kgc681r4ppgaab.apps.googleusercontent.com"
-          render={(renderProps) => (
-            <Button
-              variant="contained"
-              fullWidth
-              onClick={renderProps.onClick}
-              disabled={renderProps.disabled}
-              startIcon={<GoogleIcon />}
-            >
-              Google Sign In
-            </Button>
+          {isSignup && (
+            <div className="flex flex-col items-start gap-1">
+              <label className="font-semibold" htmlFor="name">
+                Name
+              </label>
+              <input
+                className="w-full rounded-sm text-lg p-1.5 outline-none bg-white border-[1px] border-grey border-opacity-50"
+                id="name"
+                type="text"
+                name="name"
+                placeholder="Name"
+                autoFocus
+                value={formData.name}
+                onChange={handleFormChange}
+              />
+            </div>
           )}
-          onSuccess={googleSuccess}
-          onFailure={googleFailure}
-          cookiePolicy={"single_host_origin"}
-        />
-        <Grid container justifyContent="flex-end">
-          <Grid item>
-            <Button onClick={switchMode}>
-              {isSignup
-                ? "Already have an account? Sign in"
-                : "Don't have an account? Sign Up"}
-            </Button>
-          </Grid>
-        </Grid>
-      </form>
+          <div className="flex flex-col items-start gap-1">
+            <label className="font-semibold" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="w-full rounded-sm text-lg p-1.5 outline-none bg-white border-[1px] border-grey border-opacity-50 "
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={handleFormChange}
+            />
+          </div>
+          <div className="flex flex-col items-start gap-1">
+            <label className="font-semibold" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="w-full rounded-sm text-lg p-1.5 outline-none bg-white border-[1px] border-grey border-opacity-50"
+              id="password"
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={handleFormChange}
+              value={formData.password}
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full rounded-md bg-deep-orange text-white font-semibold p-2 px-4 hover:opacity-90 transition-opacity ease-in-out duration-200 flex items-center justify-center h-11 disabled:cursor-not-allowed"
+            disabled={authDataLoading}
+          >
+            {authDataLoading ? (
+              <svg
+                class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  class="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  stroke-width="4"
+                ></circle>
+                <path
+                  class="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+            ) : isSignup ? (
+              "Sign Up"
+            ) : (
+              "Sign In"
+            )}
+          </button>
+          <GoogleLogin
+            clientId="203406370220-3gj58q2161jl5b0u07kgc681r4ppgaab.apps.googleusercontent.com"
+            render={(renderProps) => (
+              <button
+                className="w-full rounded-md bg-blue text-white font-semibold p-2 px-4 hover:opacity-90 transition-opacity ease-in-out duration-200 flex items-center justify-center gap-2 h-11 disabled:cursor-not-allowed"
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+              >
+                <FcGoogle className="w-7 h-7" />
+                Google Sign In
+              </button>
+            )}
+            onSuccess={googleSuccess}
+            onFailure={googleFailure}
+            cookiePolicy={"single_host_origin"}
+          />
+          <button
+            type="button"
+            className="font-semibold p-2"
+            onClick={switchMode}
+          >
+            {isSignup
+              ? "Already have an account? Sign in"
+              : "Don't have an account? Sign Up"}
+          </button>
+        </form>
+      </div>
     </section>
   );
 };
--- src/pages/ProductPage/ProductPage.js
+++ src/pages/ProductPage/ProductPage.js
@@ -1,41 +1,21 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect } from "react";
 import LoadingPage from "../LoadingPage/LoadingPage";
-import { useDispatch, useSelector } from "react-redux";
-import { productAddedToCart } from "../../features/Cart/cartSlice";
-import { useParams } from "react-router-dom";
-import moment from "moment";
-import useHandleAddToFavorite from "../../hooks/useHandleAddToFavorite";
-import FavoriteIcon from "@mui/icons-material/Favorite";
+import { useSelector } from "react-redux";
 
-import {
-  Button,
-  TextField,
-  Rating,
-  Box,
-  Grid,
-  Typography,
-  Divider,
-  Paper,
-  Container,
-} from "@mui/material";
-import { LoadingButton } from "@mui/lab";
+import { useNavigate, useParams } from "react-router-dom";
 import useAxios from "../../hooks/useAxios";
 import { publicAxios } from "../../api/axios";
 import { selectCurrentUser } from "../../features/user/userSlice";
 import AddReview from "../../components/AddReview/AddReview";
 import Reviews from "../../components/Reviews/Reviews";
-import Rates from "../../components/Rates/Rates";
-import Price from "../../components/Price/Price";
-import SnackbarContext from "../../context/SnackbarContext";
+
+import ProductFullDetails from "../../components/ProductFullDetails/ProductFullDetails";
+import ProductDetailsPanel from "../../components/ProductDetailsPanel/ProductDetailsPanel";
 
 const ProductPage = () => {
-  const { openSnackbar } = useContext(SnackbarContext);
-  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const params = useParams();
 
-  const [productQuantity, setProductQuantity] = useState(1);
-  const [productQuantityError, setProductQuantityError] = useState(false);
-  const [addToFavoriteLoading, handleAddToFavorite] = useHandleAddToFavorite();
   const [product, productLoading, productError, fetchProduct] = useAxios();
   const productIsFavorite = useSelector((state) =>
     state.user?.user?.favoriteProducts.includes(params.id)
@@ -50,158 +30,46 @@ const ProductPage = () => {
     });
   }, []);
 
-  const onAddToCartClick = () => {
-    if (productQuantityError || !product?.stock > 0) return;
-    dispatch(
-      productAddedToCart({ productToAdd: product, amount: productQuantity })
-    );
-    openSnackbar("Product added to cart");
-  };
-
   if (productLoading) return <LoadingPage fullHeight={true} />;
   else if (productError) return <div>Error: {productError}</div>;
   else
     return (
-      <Container sx={{ minHeight: "calc(100vh - 124px)" }}>
-        <Box sx={{ paddingTop: "1px" }}>
-          <Grid container spacing={3} sx={{ marginTop: "0" }}>
-            <Grid item xs={3}>
-              <img src={product?.imageUrl} alt={product?.description} />
-            </Grid>
-            <Grid item xs={6}>
-              <Typography gutterBottom variant="body1">
-                {product.description}
-              </Typography>
-              <Rates rates={product.rates} />
-              <Divider sx={{ margin: "5px 0" }} />
-              <Price
-                price={product.price}
-                priceAfterDiscount={product?.priceAfterDiscount}
-              />
-              <Typography variant="caption" gutterBottom>
-                All prices include VAT.
-              </Typography>
-              <Divider sx={{ margin: "5px 0" }} />
-              <Typography variant="h6">Details</Typography>
-              <Typography variant="body2">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
-                veniam ipsam cum tenetur quia modi debitis cumque tenetur quia
-                modi debitis cumque.
-              </Typography>
-            </Grid>
-            <Grid item xs={3} sx={{ padding: "10px" }}>
-              <Paper
-                variant="outlined"
-                sx={{
-                  padding: "10px",
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "flex-start",
-                }}
-              >
-                <Typography variant="h6">
-                  $
-                  {product?.priceAfterDiscount
-                    ? product.priceAfterDiscount
-                    : product.price}
-                  .00
-                </Typography>
-                <Typography variant="body1">
-                  Delivery:{" "}
-                  {moment(new Date()).add(2, "days").format("dddd, MMM.D")}
-                </Typography>
-                {!product?.stock > 0 ? (
-                  <Typography variant="h6" sx={{ color: "red" }}>
-                    Out of stock.
-                  </Typography>
-                ) : (
-                  <Typography variant="h6">In stock.</Typography>
-                )}
-                {product?.stock > 0 && (
-                  <>
-                    <TextField
-                      sx={{
-                        alignSelf: "center",
-                        width: "40%",
-                        margin: "10px 0",
-                      }}
-                      value={productQuantity}
-                      onChange={(e) => {
-                        const value = parseInt(e.target.value);
-                        setProductQuantity(value);
-                        if (value > product.stock) {
-                          setProductQuantityError(true);
-                        } else {
-                          setProductQuantityError(false);
-                        }
-                      }}
-                      error={productQuantityError}
-                      label="Qty"
-                      type="number"
-                      InputLabelProps={{
-                        shrink: true,
-                      }}
-                    />
-                    <Button
-                      variant="contained"
-                      color="secondary"
-                      sx={{ alignSelf: "center", margin: "20px 0" }}
-                      onClick={onAddToCartClick}
-                    >
-                      Add to cart
-                    </Button>
-                  </>
-                )}
-                <LoadingButton
-                  aria-label="favorite"
-                  loading={addToFavoriteLoading}
-                  onClick={() => handleAddToFavorite(params.id)}
-                  variant={productIsFavorite ? "text" : "outlined"}
-                  sx={{ alignSelf: "center" }}
+      <section className="min-h-[calc(100vh_-_theme(headerAndFooterHeight))] p-3">
+        <div className="container mx-auto flex flex-col items-center justify-center gap-3">
+          {/* Product info section */}
+          <div className="grid justify-center items-start gap-4 grid-cols-4">
+            <ProductFullDetails product={product} />
+
+            <ProductDetailsPanel
+              product={product}
+              productIsFavorite={productIsFavorite}
+            />
+          </div>
+          <hr className="text-pale-white h-px w-full" />
+          {/* Reviews section */}
+          <h3 className="text-xl font-semibold self-center my-3">Reviews</h3>
+          <div className="w-full grid grid-cols-2 gap-4">
+            {user ? (
+              <AddReview productId={params.id} />
+            ) : (
+              <p className="text-center text-lg">
+                Please{" "}
+                <button
+                  className="hover:underline underline-offset-2"
+                  onClick={() => navigate("/auth")}
                 >
-                  {productIsFavorite ? (
-                    <FavoriteIcon sx={{ color: "red" }} />
-                  ) : (
-                    <Typography variant="body2">Add to favorite</Typography>
-                  )}
-                </LoadingButton>
-              </Paper>
-            </Grid>
-            <Grid item xs={12}>
-              <Divider sx={{ margin: "5px 0" }} />
-            </Grid>
-            <Grid item xs={12}>
-              <Typography variant="h6" gutterBottom align="center">
-                Reviews
-              </Typography>
-            </Grid>
+                  login
+                </button>{" "}
+                to add a review.
+              </p>
+            )}
 
-            <Grid item xs={6}>
-              {user ? (
-                <AddReview productId={params.id} />
-              ) : (
-                <Typography variant="h6" align="center">
-                  Please login to add a review.
-                </Typography>
-              )}
-            </Grid>
-            <Grid item xs={6}>
+            <div>
               <Reviews productId={params.id} />
-            </Grid>
-
-            <Grid item xs={12}>
-              <Divider sx={{ margin: "5px 0" }} />
-            </Grid>
-
-            <Grid item xs={12}>
-              <Typography variant="subtitle1" gutterBottom>
-                Customers also viewed
-              </Typography>
-              <Box>Coming soon</Box>
-            </Grid>
-          </Grid>
-        </Box>
-      </Container>
+            </div>
+          </div>
+        </div>
+      </section>
     );
 };
 
