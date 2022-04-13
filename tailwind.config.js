80125b92489f5bcb6f394025386726e36cda0275
Mtanash <tanashkoo@gmail.com>
2022-04-13 19:57:35 +0200

    add custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    headerAndFooterHeight: "172px",
     colors: {
       "deep-orange": "#FF5F00",
       "deep-red": "#B20600",
@@ -9,7 +10,9 @@ module.exports = {
       grey: "#444",
       white: "#fff",
       red: "red",
+      blue: "#14279B",
       "pale-grey": "#8D8DAA",
+      "pale-red": "#7B5298",
     },
     extend: {},
   },
