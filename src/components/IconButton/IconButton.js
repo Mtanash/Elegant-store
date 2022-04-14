85041805295e577ecc4c25f166c614e639b37892
Mtanash <tanashkoo@gmail.com>
2022-04-14 15:24:55 +0200

    add align and color properties

--- src/components/IconButton/IconButton.js
+++ src/components/IconButton/IconButton.js
@@ -1,7 +1,11 @@
-const IconButton = ({ text, onButtonClick, Icon }) => {
+const IconButton = ({ text, onButtonClick, Icon, alignCenter, color }) => {
   return (
     <button
-      className="h-8 flex items-center gap-2 p-1 px-2 font-semibold bg-deep-orange text-white rounded-md hover:opacity-80 transition-opacity"
+      className={`h-8 flex items-center gap-2 py-2 px-4 font-semibold ${
+        color ? `bg-${color}` : "bg-deep-orange"
+      } text-white rounded-md hover:scale-95 transition-transform ${
+        alignCenter && "self-center"
+      }`}
       onClick={onButtonClick}
     >
       {text} <Icon className="inline-block w-6 h-6" />
