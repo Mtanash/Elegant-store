2c4fe23f5af6010bfbbff726fcf85e4befc236fb
Mohamed Tanash <tanashkoo@gmail.com>
2022-09-26 12:55:18 +0200

    fix styling issues

--- src/components/Price/Price.js
+++ src/components/Price/Price.js
@@ -1,7 +1,11 @@
 const Price = ({ price, priceAfterDiscount, center }) => {
   const discount = Math.floor(((price - priceAfterDiscount) / price) * 100);
   return (
-    <div className={`flex h-14 ${center ? "justify-center" : "justify-start"}`}>
+    <div
+      className={`flex h-14 ${
+        center ? "justify-center" : "justify-start"
+      } py-1`}
+    >
       {priceAfterDiscount ? (
         <div
           className={`flex flex-col justify-center ${
@@ -9,11 +13,15 @@ const Price = ({ price, priceAfterDiscount, center }) => {
           }`}
         >
           <div className="flex items-center gap-1">
-            EGP
-            <p className="text-xl font-semibold">{priceAfterDiscount}.00</p>
+            <div className="flex gap-1">
+              <p className="text-xs">EGP</p>
+              <p className="text-xl font-semibold">
+                {priceAfterDiscount.toFixed(2)}
+              </p>
+            </div>
           </div>
           <div className="flex items-center gap-[10px]">
-            <p className="line-through text-sm">EGP {price}.00</p>
+            <p className="line-through text-sm">EGP {price.toFixed(2)}</p>
             <p
               className="text-sm font-medium text-green"
               sx={{ fontSize: ".9rem", fontWeight: "500", color: "green" }}
@@ -23,9 +31,9 @@ const Price = ({ price, priceAfterDiscount, center }) => {
           </div>
         </div>
       ) : (
-        <div className="flex items-center gap-1">
-          EGP
-          <p className="text-xl font-semibold">{price}.00</p>
+        <div className="flex items-start gap-1">
+          <p className="text-xs">EGP</p>
+          <p className="text-xl font-semibold">{price.toFixed(2)}</p>
         </div>
       )}
     </div>
