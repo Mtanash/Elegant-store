85ecfe5e199ddde94abd118f93b81a5f5a6bd96b
Mohamed Tanash <tanashkoo@gmail.com>
2022-10-02 18:28:47 +0200

    add orderClick handler

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,24 +1,28 @@
 import {
-  Typography,
-  TableContainer,
+  Link,
   Table,
+  TableBody,
+  TableCell,
+  TableContainer,
   TableHead,
   TableRow,
-  TableCell,
-  TableBody,
-  Link,
 } from "@mui/material";
 import moment from "moment";
 import { useNavigate } from "react-router-dom";
 
 const Orders = ({ title, orders, showAllOrders = false }) => {
   const navigate = useNavigate();
+
+  const handleOrderClick = (orderId) => {
+    navigate(`${orderId}`);
+  };
+
   return (
     <div className="shadow-special w-full h-full p-4">
       {title && <p className="text-xl mb-4">{title}</p>}
 
       {orders?.length === 0 ? (
-        <Typography>No orders</Typography>
+        <p>No orders</p>
       ) : (
         <TableContainer>
           <Table aria-label="orders table" size="small">
@@ -35,9 +39,9 @@ const Orders = ({ title, orders, showAllOrders = false }) => {
               {orders.map((order, index) => {
                 const {
                   _id,
-                  orderInfo: { firstName, lastName },
-                  orderStatus,
-                  orderTotalPrice,
+                  info: { firstName, lastName },
+                  status,
+                  totalPrice,
                   createdAt,
                 } = order;
                 return (
@@ -50,7 +54,7 @@ const Orders = ({ title, orders, showAllOrders = false }) => {
                         backgroundColor: "#eee",
                       },
                     }}
-                    onClick={() => console.log("clicked", _id)}
+                    onClick={() => handleOrderClick(_id)}
                   >
                     <TableCell align="center">{index + 1}</TableCell>
                     <TableCell>
@@ -59,8 +63,8 @@ const Orders = ({ title, orders, showAllOrders = false }) => {
                     <TableCell>
                       {moment(createdAt).format("D MMMM YYYY")}
                     </TableCell>
-                    <TableCell>{orderStatus}</TableCell>
-                    <TableCell>{orderTotalPrice}</TableCell>
+                    <TableCell>{status}</TableCell>
+                    <TableCell>{totalPrice.toFixed(2)}</TableCell>
                   </TableRow>
                 );
               })}
