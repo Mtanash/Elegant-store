d6efce065d9b853316613e3f093037e62502dc8a
Mohamed Tanash <tanashkoo@gmail.com>
2022-10-03 12:30:54 +0200

    fix styles

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,17 +1,27 @@
-const Input = ({ label, value, onChange, readOnly, name, type, id }) => {
+const Input = ({
+  label,
+  value,
+  onChange,
+  readOnly,
+  name,
+  type,
+  id,
+  required,
+}) => {
   return (
-    <div className="flex gap-1 w-full items-center">
-      <label className="font-semibold flex-[0.2]" htmlFor={id}>
+    <div className="flex flex-col items-start gap-1 w-full">
+      <label className="font-semibold" htmlFor={id}>
         {label}
       </label>
       <input
-        className="block w-full p-2 py-3 flex-[0.8] text-base font-normal resize-none outline-none border-2 border-pale-grey rounded-md hover:border-deep-blue border-opacity-50 transition-colors"
+        className="block w-full p-2 py-3 text-base font-normal resize-none outline-none border-2 border-pale-grey rounded-md hover:border-deep-blue focus:border-blue border-opacity-50 transition-colors"
         id={id}
         type={type || "text"}
         name={name}
         value={value}
         onChange={onChange}
         readOnly={readOnly || false}
+        required={required || false}
       />
     </div>
   );
