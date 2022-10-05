612bcf12d5d36c8b6c740d347dee470d45dd8377
Mohamed Tanash <tanashkoo@gmail.com>
2022-10-06 00:51:12 +0200

    add className prop

--- src/pages/LoadingPage.js
+++ src/pages/LoadingPage.js
@@ -1,11 +1,11 @@
 import { CircularProgress } from "@mui/material";
 
-const LoadingPage = ({ fullHeight }) => {
+const LoadingPage = ({ fullHeight, className }) => {
   return (
     <section
       className={`${
         fullHeight && "min-h-[calc(100vh_-_theme(headerAndFooterHeight))]"
-      } flex justify-center items-center`}
+      } flex justify-center items-center ${className}`}
     >
       <CircularProgress />
     </section>
